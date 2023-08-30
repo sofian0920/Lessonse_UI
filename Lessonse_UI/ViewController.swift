f510c559874dcb5c3a0dab5e31515adfc10abc1b
exem <norinasofia@mail.ru>
2023-08-30 15:24:53 +0300

    Lesson 1 (create button with code, add create and activete constraint)

--- Lessonse_UI/ViewController.swift
+++ Lessonse_UI/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpView()
+        button.setTitle("Кнопка", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .green
+        
+        
     }
-
-
+    private func setUpView() {
+        //Отключаем автоматическое создание constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        //Добавляем представление к родительскому View
+        view.addSubview(button)
+        
+        //Создаем ограничения и сразу активируем их
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15)
+        ])
+    
+    }
+    
 }
-
