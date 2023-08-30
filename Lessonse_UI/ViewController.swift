93624b2b7180b8fc60f78aa6c9f1b0a11abfe24e
exem <norinasofia@mail.ru>
2023-08-30 15:41:37 +0300

    Lesson 1 (create button with code, add create and activete constraint)

--- Lessonse_UI/ViewController.swift
+++ Lessonse_UI/ViewController.swift
@@ -8,17 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    let  mainLabel = UILabel()
+    let titleLabel = UILabel()
     let button = UIButton()
+    
+    let horizontalStack = UIStackView()
+    let verticalStack = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpView()
+        mainLabel.text = "Заголовок с каким-то текстом"
+        mainLabel.textColor = .black
+        
+        
+        titleLabel.text = "Title"
+        titleLabel.textColor = .black
+        
+        
         button.setTitle("Кнопка", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .green
         
+        
+        horizontalStack.axis = .horizontal
+        verticalStack.axis = .vertical
+        setUpView()
         
     }
     private func setUpView() {
@@ -26,6 +41,20 @@ class ViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         //Добавляем представление к родительскому View
         view.addSubview(button)
+        
+        [button, titleLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            horizontalStack.addArrangedSubview($0)
+        }
+        
+        [mainLabel, horizontalStack].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            verticalStack.addArrangedSubview($0)
+        }
+        //Отключаем автоматическое создание constraints
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        //Добавляем вертикальный стек к родительскому View
+        view.addSubview(verticalStack)
         
         //Создаем ограничения и сразу активируем их
         NSLayoutConstraint.activate([
